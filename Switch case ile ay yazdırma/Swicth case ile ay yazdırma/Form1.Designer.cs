d12ade9e7274a20edadbb5958aeea23979bd3082
Berkayyildirim24 <90174527+Berkayyildirim24@users.noreply.github.com>
2021-09-09 18:04:51 +0300

    Switch-case ile ay yazdırma
    c# ile yaptığım basit bir uygulama

---
+++ Switch case ile ay yazdırma/Swicth case ile ay yazdırma/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Swicth_case_ile_ay_yazdırma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.l1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.l2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Kozuka Mincho Pro EL", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(121, 94);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Ay Getir";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // l1
+            // 
+            this.l1.AutoSize = true;
+            this.l1.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l1.Location = new System.Drawing.Point(12, 28);
+            this.l1.Name = "l1";
+            this.l1.Size = new System.Drawing.Size(85, 19);
+            this.l1.TabIndex = 1;
+            this.l1.Text = "Ay Giriniz:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(108, 30);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // l2
+            // 
+            this.l2.AutoSize = true;
+            this.l2.Font = new System.Drawing.Font("Bahnschrift Light SemiCondensed", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.l2.Location = new System.Drawing.Point(117, 63);
+            this.l2.Name = "l2";
+            this.l2.Size = new System.Drawing.Size(0, 19);
+            this.l2.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Coral;
+            this.ClientSize = new System.Drawing.Size(262, 138);
+            this.Controls.Add(this.l2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.l1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label l1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label l2;
+    }
+}
+
---
+++ Switch case ile ay yazdırma/Swicth case ile ay yazdırma/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Swicth_case_ile_ay_yazdırma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int ay = Convert.ToInt16(textBox1.Text);
+            switch (ay)
+            {
+                case 1: l2.Text = ("OCAK"); break;
+                case 2: l2.Text = ("ŞUBAT"); break;
+                case 3: l2.Text = ("MART"); break;
+                case 4: l2.Text = ("NİSAN"); break;
+                case 5: l2.Text = ("MAYIS"); break;
+                case 6: l2.Text = ("HAZİRAN"); break;
+                case 7: l2.Text = ("TEMMUZ"); break;
+                case 8: l2.Text = ("AĞUSTOS"); break;
+                case 9: l2.Text = ("EYLÜL"); break;
+                case 10: l2.Text = ("EKİM"); break;
+                case 11: l2.Text = ("KASIM"); break;
+                case 12: l2.Text = ("ARALIK"); break;
+                default: l2.Text = ("HATALI GİRİŞ"); break;
+
+            }
+        }
+    }
+}
